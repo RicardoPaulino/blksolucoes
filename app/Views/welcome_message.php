fc36b87258801783de11bff479ae4323fedbf4d0
Ricardo Paulino <ricardo.paulino@byteon.com.br>
2025-01-08 22:49:54 -0300

    merged to main

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -225,9 +225,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter <?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
-
-        <h2>The small framework with powerful features</h2>
+        <h1>Just a test</h1>
 
     </div>
 
